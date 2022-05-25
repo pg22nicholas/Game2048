6df3e83b925ca7737c0530b55c4826feae2e23f0
pg22nicholas <pg22nicholas@vfs.com>
2022-05-25 01:29:05 -0700

    Added untested logic for swiping and updating the cells inside the grid

--- app/src/main/java/com/example/game2048/Cell.kt
+++ app/src/main/java/com/example/game2048/Cell.kt
@@ -1,6 +1,11 @@
 package com.example.game2048
 
+import androidx.annotation.Nullable
 import java.util.*
 
-
-data class Cell(val value : Int, var currGridPos : Int, var nextGridPos : Int = currGridPos, var deleteFlag : Boolean = false)
+class Cell(val value : Int, var currGridPos : Vector2, var nextGridPos : Vector2 = Vector2(currGridPos.x, currGridPos.y), var deleteFlag : Boolean = false)
+{
+    init {
+        // todo: Initiate the view
+    }
+}
--- app/src/main/java/com/example/game2048/Grid.kt
+++ app/src/main/java/com/example/game2048/Grid.kt
@@ -3,16 +3,76 @@ package com.example.game2048
 class Grid() {
 
     val gridSize : Int = 4
-    var gridList = arrayListOf<Cell>()
+    var gridList = List(4) { mutableListOf<Cell?>()}
 
     init {
         // TODO: Initialize cells inside grid
+        for (i in 0..3) {
+            for (j in 0..3) {
+                gridList[i][j] = null
+            }
+        }
+
+        gridList[0][0] = Cell(2, Vector2(0, 0))
+        gridList[1][0] = Cell(2, Vector2(1, 0))
     }
 
     // register user swiping in direction and update cell positions in grid
     fun swipe(direction : Direction) {
-        // TODO: Go through each cell and calculate their next positions using their position and the nextPosition of the cell they're colliding with
-        //  Set cell boolean to delete
+        when(direction) {
+            Direction.DOWN -> {
+                for (i in 3 downTo 0) {
+                    for (j in 0..3) {
+                        findNewPosition(Vector2(j, i), Vector2(0, -1))
+                    }
+                }
+            }
+            Direction.UP -> {
+                for (i in 0..3) {
+                    for (j in 0..3) {
+                        findNewPosition(Vector2(j, i), Vector2(0, 1))
+                    }
+                }
+            }
+            Direction.RIGHT -> {
+                for (i in 3 downTo 0) {
+                    for (j in 0..3) {
+                        findNewPosition(Vector2(i, j), Vector2(1, 0))
+                    }
+                }
+            }
+            Direction.LEFT -> {
+                for (i in 0..3) {
+                    for (j in 0..3) {
+                        findNewPosition(Vector2(i, j), Vector2(-1, 0))
+                    }
+                }
+            }
+        }
+    }
+
+    fun findNewPosition(pos : Vector2, direction: Vector2) {
+        if (gridList[pos.x][pos.y] == null) return
+
+        val nextPos : Vector2 = Vector2(pos.x + direction.x, pos.y + direction.y)
+
+        // bottom of grid
+        if (pos.y == 3) return
+        // If next position if null
+        if (gridList[nextPos.x][nextPos.y] == null) {
+            gridList[nextPos.x][nextPos.y] = gridList[pos.x][pos.y]
+            gridList[pos.x][pos.y] = null
+            findNewPosition(Vector2(nextPos.x, nextPos.y), direction)
+            // if next position same value, combine
+        } else if (gridList[pos.x][pos.y]?.value == gridList[nextPos.x][nextPos.y]?.value) {
+            val cellToDelete = gridList[nextPos.x][nextPos.y]
+            cellToDelete?.deleteFlag = true
+            gridList[nextPos.x][nextPos.y] = gridList[pos.x][pos.y]
+            gridList[nextPos.x][nextPos.y]?.nextGridPos = Vector2(nextPos.x, nextPos.y)
+        // otherwise, next position different value, do nothing
+        } else {
+            return
+        }
     }
 
     // Notifies grid that the animations were all finished and the cell's current positions should be updated
